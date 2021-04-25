ff158c242c998a3786ca168f41011e1597f0926c
mariamalshammari <malshammari37@gmail.com>
2021-04-25 15:53:55 +0300

    demo

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 let userName = prompt('what\'s your name');
-console.log(userName);
+console.log('Your name is:'+ userName);
 
 let color = prompt('what\'s your fav color');
-console.log(color);
+console.log('your fav color is :'+ color);
 
 let age = prompt('whats your age');
-console.log(age);
+console.log(' your age is '+ age);
 
-alert('Hello ' + userName + ' your fav color is ' + color + ' and your age is ' + age);
+let fruit = prompt('whats your fav fruit');
+console.log('and your fav fruit is :'+ fruit);
+
+alert('Hello ' + userName + ' your fav color is ' + color + ' ,your age is ' + age +'and your fav fruit is :'+ fruit);
