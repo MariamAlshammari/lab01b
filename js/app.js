2ed697a0b4bd2ebfabfd4dfaf6dccaf64a21352a
mariamalshammari <malshammari37@gmail.com>
2021-04-25 14:32:37 +0300

    demo 1

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let userName = prompt('what\'s your name');
+console.log(userName);
+
+let color = prompt('what\'s your fav color');
+console.log(color);
+
+let age = prompt('whats your age');
+console.log(age);
+
+alert('Hello ' + userName + ' your fav color is ' + color + ' and your age is ' + age);
